0a7f0c30293a0cab6bdf569182ed5f76caf42d0f
Ploythun <ploytwrsrijandon@gmail.com>
2026-02-13 00:51:38 +0700

    Lab18 complete

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,19 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1, Rect *R2){
+	double overlapxstart,overlapxend,overlapx;
+	double overlapy,overlapystart,overlapyend;
+	
+	overlapxstart = (R1->x > R2->x ? R1->x : R2->x);
+	overlapxend = (R1->x+R1->w < R2->x+R2->w ? R1->x+R1->w : R2->x+R2->w);
+	overlapx = overlapxend - overlapxstart;
+	if(overlapx <= 0) return 0;
 
+	overlapystart = (R1->y-R1->h > R2->y-R2->h ? R1->y-R1->h : R2->y-R2->h);
+	overlapyend = (R1->y < R2->y ? R1->y : R2->y);
+	overlapy = overlapyend - overlapystart;
+	if(overlapy <= 0) return 0;
 
-
-}
+	return overlapx * overlapy;
+}			
